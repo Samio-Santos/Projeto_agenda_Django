39c9a4cc4e4733fee41edd3e93ff49ccc9d6e839
Samio Santos <71763791+Samio-Santos@users.noreply.github.com>
2021-01-11 06:14:31 -0300

    update

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from contatos.models import Contato
-# from django.contrib.auth.models import User
-# from django.contrib.auth import admin as auth_admin
+from django.contrib.auth.models import User
+from django.contrib.auth import admin as auth_admin
 
 # Create your models here.
 
@@ -9,8 +9,8 @@ class Perfil(models.Model):
     first_name = models.CharField(max_length=35, blank=False, null=False)
     last_name = models.CharField(max_length=35, blank=False, null=False)
     email = models.EmailField(max_length=45, blank=True, null=True)
-    bio = models.TextField(blank=True)
-    imagem = models.ImageField(blank=True, upload_to='fotoPerfil')
+    # bio = models.TextField(blank=True)
+    # imagem = models.ImageField(blank=True, upload_to='fotoPerfil')
 
     def __str__(self):
         return self.first_name
